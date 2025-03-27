d46a4a786ac3b8cff7b78a347d161df0bc94b94c
LP203 <notapad@hotmail.com>
2025-03-27 15:48:12 +0000

    Code Update

---
+++ rijndael.h
@@ -0,0 +1,21 @@
+/*
+	Lorenzo Palleschi D24126922
+	This header file declares the public interface for 
+	the AES-128 encryption and decryption functions.
+ */
+
+#ifndef RIJNDAEL_H
+#define RIJNDAEL_H
+
+#define BLOCK_ACCESS(block, row, col) (block[(row * 4) + col])
+#define BLOCK_SIZE 16
+
+/*
+ * These should be the main encrypt/decrypt functions (i.e. the main
+ * entry point to the library for programmes hoping to use it to
+ * encrypt or decrypt data)
+ */
+unsigned char *aes_encrypt_block(unsigned char *plaintext, unsigned char *key);
+unsigned char *aes_decrypt_block(unsigned char *ciphertext, unsigned char *key);
+
+#endif
