ca5176370e0fbb5c667771392c57f3edfd250087
LP1911 <D24126922@mytudublin.ie>
2025-03-30 00:17:21 +0000

    Update to row shifting
    Corrected the row shifting by matching the matrix indexing which python uses.

--- rijndael.c
+++ rijndael.c
@@ -61,38 +61,40 @@ void sub_bytes(unsigned char *block) {
     }
 }
 
+// Rotates the 4 bytes in each row each shift
 void shift_rows(unsigned char *block) {
-    unsigned char temp[16];
+  unsigned char temp[16];
 
-    // Row 0 Isn't shifted
-    temp[0] = block[0];
-    temp[1] = block[1];
-    temp[2] = block[2];
-    temp[3] = block[3];
+  // Row 0: Isn't shifted
+  temp[0]  = block[0];
+  temp[4]  = block[4];
+  temp[8]  = block[8];
+  temp[12] = block[12];
 
-    // Row 1 is shifted left by 1
-    temp[4] = block[5];
-    temp[5] = block[6];
-    temp[6] = block[7];
-    temp[7] = block[4];
+  // Row 1: is shifted left by 1
+  temp[1]  = block[5];
+  temp[5]  = block[9];
+  temp[9]  = block[13];
+  temp[13] = block[1];
 
-    // Row 2 is shifted left by 2
-    temp[8] = block[10];
-    temp[9] = block[11];
-    temp[10] = block[8];
-    temp[11] = block[9];
+  // Row 2: is shifted left by 2
+  temp[2]  = block[10];
+  temp[6]  = block[14];
+  temp[10] = block[2];
+  temp[14] = block[6];
 
-    // Row 3 is shifted left by 3
-    temp[12] = block[15];
-    temp[13] = block[12];
-    temp[14] = block[13];
-    temp[15] = block[14];
+  // Row 3: is shifted left by 3
+  temp[3]  = block[15];
+  temp[7]  = block[3];
+  temp[11] = block[7];
+  temp[15] = block[11];
 
-    // temp is copied back into the block
-    for (int i = 0; i < 16; i++) {
-        block[i] = temp[i];
-    }
+  // Temp is copied back into block
+  for (int i = 0; i < 16; i++) {
+      block[i] = temp[i];
+  }
 }
+
 
 void mix_columns(unsigned char *block) {
   // TODO: Implement me!
@@ -110,37 +112,38 @@ void invert_sub_bytes(unsigned char *block) {
 }
 
 void invert_shift_rows(unsigned char *block) {
-    unsigned char temp[16];
+  unsigned char temp[16];
 
-    // Row 0: Isn't shifted
-    temp[0] = block[0];
-    temp[1] = block[1];
-    temp[2] = block[2];
-    temp[3] = block[3];
+  // Row 0: Isn't shifted
+  temp[0]  = block[0];
+  temp[4]  = block[4];
+  temp[8]  = block[8];
+  temp[12] = block[12];
 
-    // Row 1 is shifted right by 1
-    temp[4] = block[7];
-    temp[5] = block[4];
-    temp[6] = block[5];
-    temp[7] = block[6];
+  // Row 1: is shifted right by 1
+  temp[1]  = block[13];
+  temp[5]  = block[1];
+  temp[9]  = block[5];
+  temp[13] = block[9];
 
-    // Row 2 is shifted right by 2
-    temp[8] = block[10];
-    temp[9] = block[11];
-    temp[10] = block[8];
-    temp[11] = block[9];
+  // Row 2: is shifted right by 2
+  temp[2]  = block[10];
+  temp[6]  = block[14];
+  temp[10] = block[2];
+  temp[14] = block[6];
 
-    // Row 3 is shifted right by 3
-    temp[12] = block[13];
-    temp[13] = block[14];
-    temp[14] = block[15];
-    temp[15] = block[12];
+  // Row 3: is shifted right by 3
+  temp[3]  = block[7];
+  temp[7]  = block[11];
+  temp[11] = block[15];
+  temp[15] = block[3];
 
-    // temp is copied back into the block
-    for (int i = 0; i < 16; i++) {
-        block[i] = temp[i];
-    }
+  // Temp is copied back into block
+  for (int i = 0; i < 16; i++) {
+      block[i] = temp[i];
+  }
 }
+
 
 void invert_mix_columns(unsigned char *block) {
   // TODO: Implement me!
